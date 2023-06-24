f1431951efcb677a1050d0ca04f26fc5d2a7e8b0
nombre de usuariofalcaob <irene.falcaob@gmail.com>
2023-06-25 01:24:07 +0200

    Entities 25/06

---
+++ src/main/java/com/example/library/models/entities/Libro.java
@@ -0,0 +1,98 @@
+package com.example.library.models.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "libros")
+public class Libro implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotEmpty
+	@Size(min = 3, max = 35)
+	private String titulo;
+	
+	@NotEmpty
+	@Size(min = 3, max = 25)
+	private String autor;
+	
+	@NotEmpty
+	@Size(min = 3, max = 25)
+	private Long ejemplares;
+	
+	@NotEmpty
+	@Size(min = 3, max = 15)
+	private String genero;
+	
+	@NotEmpty
+	@Size(min = 3, max = 10)
+	private Long ISBN;
+	
+	
+	// getters y setters
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public Long getEjemplares() {
+		return ejemplares;
+	}
+
+	public void setEjemplares(Long ejemplares) {
+		this.ejemplares = ejemplares;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public Long getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(Long iSBN) {
+		ISBN = iSBN;
+	}
+
+}
---
+++ src/main/java/com/example/library/models/entities/Usuario.java
@@ -0,0 +1,106 @@
+package com.example.library.models.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+//import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotEmpty
+	@Size(min = 3, max = 10)
+	private String nombre;
+	
+	@NotEmpty
+	@Size(min = 3, max = 15)
+	private String apellido;
+	
+	@Column(name = "create_at")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@NotNull
+	private LocalDate fechaNacimiento;
+	
+	@NotEmpty
+	@Email
+	private String email;
+	
+	/*
+	private List<Libro> libros;
+	
+	@ManyToMany
+    @JoinTable(
+        name = "usuario_libro",
+        joinColumns = @JoinColumn(name = "usuario_id"),
+        inverseJoinColumns = @JoinColumn(name = "libro_id")
+    )
+	*/
+	
+	
+	// getters and setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+
+}
